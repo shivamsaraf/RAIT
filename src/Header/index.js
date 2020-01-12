12188042d256b9349ecd280ed29d4675ec11602e
unknown <asd.shivam98@gmail.com>
2020-01-12 15:36:03 +0530

    made header responsive

--- src/Header/index.js
+++ src/Header/index.js
@@ -5,19 +5,26 @@ export default class Header extends React.Component{
     render(){
         return (
             <div>
-                <div class="flex-container">
-                <div class="image">
-                <a href="https://www.iitism.ac.in/" target="_blank"><img class="img" src="https://www.iitism.ac.in/assets/img/ismlogo.png" /></a>
-                </div>
-                <div class="flex-container2">
-                    <div class="L1">RAIT 2020</div>
-                    <div class="L2">5th International Conference on Recent Advances in Information Technology</div><br />
-                    <div class="L3">December 11-12, 2020 | Dhanbad, India</div>
-                </div>
-                <div class="flex-container3">
-                    
-                </div>
-                </div>
+	            <div>
+	                <div className="flex-container1">
+				         <div className="image">
+				            
+				            <a href="https://www.iitism.ac.in/" target="_blank" rel="noreferrer">
+				              <img
+				                className="img"
+				                src="https://www.iitism.ac.in/assets/img/ismlogo.png"
+				                alt=""
+				              />
+				            </a>
+				          </div>
+				          <div className="flex-container2">
+				            <div className="L1">RAIT 2020</div>
+				            <div className="L2">5th International Conference on Recent Advances in Information Technology</div>
+				            <div className="L3">December 11-12, 2020 | Dhanbad, India</div>
+				          </div>
+				    </div>
+	            </div>
+               
             </div>
         )
     }
